2b21746284fd61f7ead1c43f6926656b17b08f8a
cemrtkn <cemrtkn@gmail.com>
2024-10-31 15:36:09 +0100

    numpy torch compatibality issue

--- src/nn_training.py
+++ src/nn_training.py
@@ -2,6 +2,8 @@ import numpy as np
 from config import hdf5_path
 from preprocessing import DataPreprocessor
 from model_utils import *
+import torch
+import torch.nn as nn
 
 data_preprocessor = DataPreprocessor(hdf5_path)
 #data_splits = data_preprocessor.prepare_data()
@@ -10,3 +12,13 @@ data_splits = data_preprocessor.load_pprocessed_data()
 x_train, y_train, x_val, y_val, x_test, y_test = data_splits['x_train'], data_splits['y_train'], data_splits['x_val'], data_splits['y_val'], data_splits['x_test'], data_splits['y_test']
 
 print(x_train.shape, y_train.shape, x_val.shape, y_val.shape, x_test.shape, y_test.shape)
+
+
+model = nn.Sequential(
+    nn.Linear(764, 100),
+    nn.ReLU(),
+    nn.Linear(100, 50),
+    nn.ReLU(),
+    nn.Linear(50, 10),
+    nn.Sigmoid()
+)
